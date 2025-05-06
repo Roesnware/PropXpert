c05b11be520533acf740729520b241fea3b7e461
Roesnware <bubwub04@gmail.com>
2025-05-05 23:19:22 -0400

    updated next config file

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: true,
+    env: {
+      ODDS_API_KEY: process.env.ODDS_API_KEY, // API key
+      INTERNAL_API_BASE: process.env.INTERNAL_API_BASE, // backend
+    },
+    images: {
+      domains: ['examplecdn.com', 'anotherdomain.com'], // player/team images
+    },
   }
   
+  module.exports = nextConfig
